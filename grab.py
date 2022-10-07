d7dad5eb42451b7fe2df12befcec4c6ee08830da
cruzh3r <68588947+cruzh3r@users.noreply.github.com>
2022-10-07 23:27:35 +0700

    Create grab.py

---
+++ grab.py
@@ -0,0 +1,17 @@
+import datetime
+import requests, os, sys
+from re import findall as reg
+from time import sleep
+from pyfiglet import Figlet
+
+
+custom_fig = Figlet(font='graffiti')
+
+	peg(fname,pp)
+
+sdate = raw_input(' Input start date: ')
+edate = raw_input(' Input end date: ')
+pages = raw_input(' Input page: ')
+
+if pages:
+	forr(sdate,edate,pages)
